e253d0f9d759b6bedae4a25b9a072dbf6279bad7
Colin <tresscd@gmail.com>
2019-02-26 21:27:46 -0500

    Update car class.

--- car.js
+++ car.js
@@ -1,13 +1,30 @@
 class Car {
-  constructor(floors, cap) {
-    this.floors = floors;
-    this.cap = cap;
-    this.direction = "idle";
+  constructor(floors, capacity) {
+    this.buttons = {};
+
+    for (let i = 0; i < floors.length; i++) {
+      let floor = floors[i];
+      this.buttons[floor] = false;
+    }
+
+    this.capacity = capacity;
+    this.direction = "idle"; // other states are "up" and "down"
+    this.riders = [];
   }
 
   updateDirection(direction) {
     this.direction = direction;
   }
 
-  
+  isFull() {
+    return this.riders.length === this.capacity;
+  }
+
+  pressButton(button) {
+    this.buttons[button] = true;
+  }
+
+  resolveButton(button) {
+    this.buttons[button] = false;
+  }
 }
